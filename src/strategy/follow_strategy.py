7f9fe8b79aa2f15e5af1dca4883329f824c06eea
Isha Dijcks <ishadijcks@gmail.com>
2024-11-17 15:03:48 +0100

    Make slightly less stupid strategy moderatly less stupid

--- src/strategy/follow_strategy.py
+++ src/strategy/follow_strategy.py
@@ -1,4 +1,5 @@
 from communication.commands.commands import BaseCommand, StickId
+from communication.commands.rotate_command import RotateCommand
 from communication.commands.translate_command import TranslateCommand
 from model.game_state import GameState
 from strategy.strategy import Strategy
@@ -9,10 +10,17 @@ class FollowStrategy(Strategy):
     Tries to place the keeper at the position of the ball
     """
 
+    SHOT_DELAY = 0.5
+    """How many seconds to wait before moving back to center after shooting"""
+
     def __init__(self):
         super().__init__()
+        self.total_time = 0
+        self.last_shot_time = 0
 
     def execute(self, previous_state: GameState, state: GameState, delta: float) -> list[BaseCommand]:
+        self.total_time += delta
+
         commands = []
         ball_x, ball_y = state.ball
 
@@ -30,5 +38,22 @@ class FollowStrategy(Strategy):
 
             target_position = clipped_ball / 0.33
             commands.append(TranslateCommand(StickId.ONE, target_position))
+
+        # Don't rotate for SHOT_DELAY after shooting
+        if self.last_shot_time - self.total_time < self.SHOT_DELAY:
+            return commands
+
+        # Shoot or prepare to
+
+        if ball_x < 0.13:
+            # SHOOT
+            commands.append(RotateCommand(StickId.ONE, 1))
+            self.last_shot_time = self.total_time
+        elif ball_x < 0.25:
+            # Prepare to shoot
+            commands.append(RotateCommand(StickId.ONE, 0))
+        else:
+            # Return to center
+            commands.append(RotateCommand(StickId.ONE, 0.5))
 
         return commands
